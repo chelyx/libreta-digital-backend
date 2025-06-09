17d15c5e46bc7ffd5940fd4ef21ba93c69bd9be6
araceli <asoffulto@frba.utn.edu.ar>
2025-06-09 19:42:34 -0300

    fixes

--- src/main/java/com/g5311/libretadigital/controller/GeneralController.java
+++ src/main/java/com/g5311/libretadigital/controller/GeneralController.java
@@ -3,6 +3,7 @@ package com.g5311.libretadigital.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.g5311.libretadigital.model.dto.AsignarMateriaDto;
 import com.g5311.libretadigital.service.GeneralService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class GeneralController {
@@ -30,6 +32,7 @@ public class GeneralController {
 
     @GetMapping("/hello")
     public String privateHello(@AuthenticationPrincipal Jwt jwt) {
+        System.out.println("JWT Claims: " + jwt.getClaims());
         return "Hola " + jwt.getClaimAsString("email") + ", estás autenticado!";
     }
 }
