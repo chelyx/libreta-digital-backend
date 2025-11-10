47313a08d8be12727d253ce4c48ee2962e42e4aa
Maximiliano de la Fuente <maxiidelafuente16@gmail.com>
2025-11-10 20:54:37 -0300

    Busqueda de curso segun el rol V1

--- src/main/java/com/g5311/libretadigital/repository/CursoRepository.java
+++ src/main/java/com/g5311/libretadigital/repository/CursoRepository.java
@@ -1,7 +1,12 @@
 package com.g5311.libretadigital.repository;
 
 import com.g5311.libretadigital.model.Curso;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +18,6 @@ public interface CursoRepository extends JpaRepository<Curso, UUID> {
     Optional<Curso> findByCodigoIgnoreCase(String codigo);
 
     boolean existsByCodigoIgnoreCase(String codigo);
+
+    Optional<Curso> findByCodigoAndFecha(String codigo, LocalDate fecha);
 }
